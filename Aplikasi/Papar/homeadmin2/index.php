dfb62cb3bb3463b54ad9c4017f221556d9cb6051
Amin007 Ledang <helmi007@yahoo.com>
2018-07-08 12:53:11 +0800

    susun balik koding

--- Aplikasi/Papar/homeadmin2/index.php
+++ Aplikasi/Papar/homeadmin2/index.php
@@ -8,34 +8,27 @@ include 'include/menu_atas.php';
 <!-- Page Content -->
 <div class="container">
 <div class="row">
-
-  <div class="col-lg-3"><?php include 'include/menu_right.php'; ?></div>
-  <!-- /.col-lg-3 -->
-
-  <div class="col-lg-9">
-    
-    <div class="card mt-4">
-    <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
-      <div class="card-body">
-      <h3 class="card-title">Scrapping</h3>
-      <h4>$24.99</h4>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
-      <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>4.0 stars
-      </div>
-    </div>
-  <!-- /.card -->
-
-    <!-- div class="card card-outline-secondary my-4">
-      <div class="card-header">Product Reviews</div>
-      
-    </div>
-!-- /.card -->
-
-  </div>
- <!-- /.col-lg-9 -->
-
+	<div class="col-lg-3"><?php include 'include/menu_right.php'; ?></div>
+	<!-- /.col-lg-3 -->
+	<div class="col-lg-9">
+		<div class="card mt-4">
+			<img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
+			<div class="card-body">
+			<h3 class="card-title">Scrapping</h3>
+			<h4>$24.99</h4>
+			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. 
+			Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
+			<span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>4.0 stars
+			</div>
+		</div>
+		<!-- /.card -->
+		<!-- div class="card card-outline-secondary my-4">
+		<div class="card-header">Product Reviews</div>
+		</div>
+		!-- /.card -->
+	</div>
+	<!-- /.col-lg-9 -->
 </div>
-
 </div>
 <!-- /.container -->
 <?php include 'include/dibawah.php'; ?>
