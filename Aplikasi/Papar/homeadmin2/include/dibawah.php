38f2046383ed7a8e46e9c02320fec2b67d188609
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-15 19:11:24 +0800

    susun balik koding

--- Aplikasi/Papar/homeadmin2/include/dibawah.php
+++ Aplikasi/Papar/homeadmin2/include/dibawah.php
@@ -1,15 +1,13 @@
-<!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Online Shopping Recommendation Website</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+<!-- Footer --
+<footer class="py-3 bg-dark">
+	<div class="container">
+	<p class="m-0 text-center text-white">Online Shopping Recommendation Website</p>
+</div>
+</footer> -->
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="<?php echo $pautan?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo $pautan?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script src="<?php echo $pautan?>vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo $pautan?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  </body>
-
+</body>
 </html>
