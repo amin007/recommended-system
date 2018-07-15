79c72d94beb874d65e30d761be309b145e992f18
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-15 21:33:18 +0800

    tukar paparan baru

--- Aplikasi/Papar/homeadmin2/index.php
+++ Aplikasi/Papar/homeadmin2/index.php
@@ -12,27 +12,6 @@ include 'include/menu_atas.php';
 	<!-- /.col-lg-3 -->
 	<div class="col-lg-9">
 		<div class="card mt-4">
-			<img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
-			<div class="card-body">
-			<h3 class="card-title">Scrapping</h3>
-			<h4>$24.99</h4>
-			<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. 
-			Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
-			<span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>4.0 stars
-			</div>
-		</div>
-		<!-- /.card -->
-		<!-- div class="card card-outline-secondary my-4">
-		<div class="card-header">Product Reviews</div>
-		</div>
-		!-- /.card -->
-	</div>
-	<!-- /.col-lg-9 -->
-  <div class="col-lg-3"><?php include 'include/menu_right.php'; ?></div>
-  <!-- /.col-lg-3 -->
-
-	<div class="col-lg-9">
-		<div class="card mt-4">
 			<div class="card-body">
 				<h3 class="card-title">Web Scrapping</h3>
 				<p class="card-text">Web Scrapping is a technique employed to extract large 
@@ -50,10 +29,6 @@ include 'include/menu_atas.php';
 				Fdez-Riverola, F., 2014.</p>
 			</div>
 		</div><!-- /.card -->
-<!-- div class="card card-outline-secondary my-4">
-<div class="card-header">Product Reviews</div>
-</div>
-!-- /.card -->
 	</div><!-- /.col-lg-9 -->
 
 </div>
